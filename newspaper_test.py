16fffc0cfa9e1e0cfd10ab2de3afdfeb3561e637
VedhaSankar <vedhasankar26@gmail.com>
2021-12-07 14:49:50 +0530

    prediction for rss feed done

--- newspaper_test.py
+++ newspaper_test.py
@@ -1,10 +1,23 @@
 from newspaper import Article
+import feedparser
+from validate import fake_news_det as detect
 
-url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
+
+# url = 'https://timesofindia.indiatimes.com/india/parliament-winter-session-live-updates/liveblog/88113566.cms'
+url = input ("Enter url: ")
 article = Article(url)
 
 article.download()
 
 article.parse()
 
-print (article.text)
+content = article.text
+
+print (detect(content))
+
+
+# NewsFeed = feedparser.parse(url)
+# entry = NewsFeed.entries
+
+# # print (entry.keys())
+# print (entry)
