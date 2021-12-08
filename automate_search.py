b4bb7e55f266d6e0635be6861b6e4f2bbe663bf3
VedhaSankar <vedhasankar26@gmail.com>
2021-12-08 08:43:26 +0530

    driver path added

--- automate_search.py
+++ automate_search.py
@@ -1,17 +1,20 @@
 from selenium import webdriver
-  
-# Taking input from user
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DRIVER_PATH = os.environ.get('DRIVER_PATH')   
+
+chrome_driver = DRIVER_PATH
+
+browser = webdriver.Chrome(executable_path=DRIVER_PATH)
+
 search_string = input("Input the URL or string you want to search for:")
   
-# This is done to structure the string 
-# into search url.(This can be ignored)
+
 search_string = search_string.replace(' ', '+') 
   
-# Assigning the browser variable with chromedriver of Chrome.
-# Any other browser and its respective webdriver 
-# like geckodriver for Mozilla Firefox can be used
-chrome_driver=r"C:\Users\Rahaarum\Desktop\driver\chromedriver"
-browser = webdriver.Chrome(chrome_driver)
     
 for i in range(1):
     matched_elements = browser.get("https://www.google.com/search?q=" +search_string + "&start=" + str(i))
