5b04e6876349e75d3ed4ce6abc6bf0933442bbe0
Ishita <ishitagops12@gmail.com>
2022-03-11 09:57:19 +0530

    changes made

--- image-to-text.py
+++ image-to-text.py
@@ -2,24 +2,19 @@
 from PIL import Image
 from pytesseract import pytesseract
   
-# Defining paths to tesseract.exe 
-# and the image we would be using
-path_to_tesseract = r"/home/ishita/miniconda3/envs/ml38/bin/pytesseract"
-image_path = r"/home/ishita/proj/fake-online-content-detection/test2.jpg"
+
+# path_to_tesseract = r"/home/ishita/miniconda3/envs/ml38/bin/pytesseract"
+
+image_path = "test2.jpg"
   
-# Opening the image & storing it in an image object
 img = Image.open(image_path)
+
   
-# Providing the tesseract 
-# executable location to pytesseract library
-pytesseract.tesseract_cmd = path_to_tesseract
-  
-# Passing the image object to 
-# image_to_string() function
-# This function will
+
+# pytesseract.tesseract_cmd = path_to_tesseract
+
 # extract the text from the image
 text = pytesseract.image_to_string(img)
   
-# Displaying the extracted text
 print(text[:-1])
 
