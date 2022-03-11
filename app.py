5d7d28df3d2354fd8c900cce40c5a44bca01cb31
VedhaSankar <vedhasankar26@gmail.com>
2022-03-11 12:23:37 +0530

    added route that detects whether image content is authentic or not

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request, url_for, redirect, render_template, request
+from flask import Flask,request, flash, redirect, render_template, request
 import pickle
 from score import predict_link_score as detect_link
 from score import predict_content_score as detect_content
@@ -10,6 +10,8 @@ from email.mime.application import MIMEApplication
 import os
 from flask_mail import Mail, Message
 from emailer import send_email
+from werkzeug.utils import secure_filename
+from text_extraction import image_to_string
 
 app = Flask(__name__)
 mail= Mail(app)
@@ -29,13 +31,25 @@ SENDER_ADDRESS  = os.environ.get('GMAIL_USER')
 SENDER_PASS     = os.environ.get('GMAIL_PASSWORD')
 EMAIL_LIST      = os.environ.get('EMAIL_LIST').split(',')
 
+
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = SENDER_ADDRESS
 app.config['MAIL_PASSWORD'] = SENDER_PASS
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
+app.config["UPLOAD_FOLDER"] = "uploads/"
 mail = Mail(app)
+
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+ALLOWED_EXTENSIONS  = {'png', 'jpg', 'jpeg'}
+
+
+def allowed_file(filename):
+    
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/', methods = ["GET","POST"])
@@ -69,6 +83,35 @@ def alert():
             )
 
     return render_template("index.html")
+
+@app.route('/image-detection', methods = ["GET", "POST"])
+def image_detection():
+
+    if request.method == 'POST':
+
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            text = image_to_string(filename)
+            prediction = detect_content(text)
+            return render_template('image-detection.html', prediction=prediction)
+
+        else:
+            flash("Allowed file type is {'png', 'jpg', 'jpeg'}")
+            return redirect(request.url)
+
+    return render_template('image-detection.html')
+
 
 if __name__ == "__main__":
 
