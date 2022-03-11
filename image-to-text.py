2dfa3b6fcf29d9d0d986d8703ae61121fe53b1c4
Ishita <ishitagops12@gmail.com>
2022-03-11 08:52:55 +0530

    image to text in progress

--- image-to-text.py
+++ image-to-text.py
@@ -1,25 +1,11 @@
-# from PIL import Image
-# import pytesseract
-# import numpy as np
-# import cv2
-# import os
-
-
-# filename = 'image.png'
-# img1 = np.array(Image.open(filename))
-# text = pytesseract.image_to_string(img1)
-
-
-# print(text)
-
 
 from PIL import Image
 from pytesseract import pytesseract
   
 # Defining paths to tesseract.exe 
 # and the image we would be using
-path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-image_path = r"csv\d.jpg"
+path_to_tesseract = r"/home/ishita/miniconda3/envs/ml38/bin/pytesseract"
+image_path = r"/home/ishita/proj/fake-online-content-detection/test2.jpg"
   
 # Opening the image & storing it in an image object
 img = Image.open(image_path)
@@ -36,3 +22,4 @@ text = pytesseract.image_to_string(img)
   
 # Displaying the extracted text
 print(text[:-1])
+
